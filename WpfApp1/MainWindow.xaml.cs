fb2849e7840d5d6900928811be61d693b084ec99
ChikkJa <Jayaprakash.ChikkavannagthihalliHalappa@lamresearch.com>
2021-10-05 07:07:30 +0530

    new project

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void ButtonAddName_Click(Object sender, EventArgs args)
+        {
+            if (!string.IsNullOrWhiteSpace(txtName.Text) && !lstNames.Items.Contains(txtName.Text))
+            {
+                lstNames.Items.Add(txtName.Text);
+                txtName.Clear();
+
+            }
+          
+        }
+        void OnClick1(object sender, RoutedEventArgs e)
+        {
+            btn1.Background = Brushes.LightBlue;
+        }
+
+        void OnClick2(object sender, RoutedEventArgs e)
+        {
+            btn2.Background = Brushes.Pink;
+        }
+
+        void OnClick3(object sender, RoutedEventArgs e)
+        {
+            btn1.Background = Brushes.Pink;
+            btn2.Background = Brushes.LightBlue;
+        }
+
+        
+    }
+}
